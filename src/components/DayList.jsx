a09c8d45d8cf73ecf2487a41f30b7d416756b30e
Ryan <ryan.maceachern@gmail.com>
2022-03-22 22:08:28 -0700

    corrected an error with daylist component not rendering properly

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 export default function DayList(props) {
-  const getDays = props.days.map(() => {
+  const daysInfo = props.days.map((days) => {
     return (
       <DayListItem
-        key={match.matchNumber}
-        players={match.players}
-        winner={match.winner}
-        scoreDifference={match.scoreDifference}
+        key={days.id}
+        name={days.name}
+        spots={days.spots}
+        selected={days.selected}
+        setDay={days.setDay}
       />
     );
   })
   
   return (
     <ul>
-      
+      {daysInfo}
     </ul>
   );
 }
