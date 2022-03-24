399f05607eb5f2046f58bbf58adb17e6b7adbc5f
Ryan <ryan.maceachern@gmail.com>
2022-03-23 18:21:33 -0700

    created Header and Empty components + stories

--- stories/index.js
+++ stories/index.js
@@ -5,6 +5,8 @@ import DayListItem from "components/DayListItem";
 import InterviewerList from "components/InterviewerList";
 import InterviewerListItem from "components/InterviewerListItem";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -137,6 +139,6 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />);
-
-          
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);          
