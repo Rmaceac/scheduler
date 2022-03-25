3318f4d2fa7b7a430c4a554f5d1435a72fdce990
Ryan <ryan.maceachern@gmail.com>
2022-03-24 22:02:48 -0700

    created first selector to pass all tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function getAppointmentsForDay(state, selectedDay) {
+  const aptIDs = [];
+  
+  for (const day of state.days) {
+    if (day.name === selectedDay) {
+      aptIDs.push(...day.appointments)
+    }
+  }
+  
+  const results = aptIDs.map(id => {
+      return state.appointments[id]
+    })
+          
+  return results;
+}
