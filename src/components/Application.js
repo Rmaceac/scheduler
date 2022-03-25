3c5952da17101bc69a068826d18e491b3f71db99
Ryan <ryan.maceachern@gmail.com>
2022-03-24 22:59:19 -0700

    grouped multiple states together within Application. Small bug remains where appointment data isn't being reflected from axios calls

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,63 +2,75 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 import InterviewerList from "./InterviewerList";
 
 import "../styles/Application.scss";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+// OLD HARD-CODED DATA
+// const appointments = {
+//   "1": {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   "2": {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer:{
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   "3": {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   "4": {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer:{
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   "5": {
+//     id: 5,
+//     time: "4pm",
+//   }
+// };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday")
-  const [days, setDays] = useState([]);
+  //how does this work?
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  // how do these work?
+  const setDay = day => setState({ ...state, day })
+  // const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    axios.get("/api/days")
-      .then((response) => {
-        console.log("response:", response.data)
-        setDays([...response.data])
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments")
+    ]).then((all) => {
+        console.log("Promises resolved:", all)
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
       })
   }, [])
 
-  console.log("DAY:", day);
-  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -70,8 +82,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
       <DayList
-        days={days}
-        value={day}
+        days={state.days}
+        value={state.day}
         onChange={setDay}
       />
       </nav>
@@ -82,7 +94,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map((apt) => {
+        {dailyAppointments.map((apt) => {
           return (
             <Appointment
               key={apt.id}
