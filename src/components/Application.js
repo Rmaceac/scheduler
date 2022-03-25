a646e938f6ff753201390c0fdf24bc1789876d94
Ryan <ryan.maceachern@gmail.com>
2022-03-24 20:34:10 -0700

    replaced hardcoded days data with axios request to db

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import InterviewerList from "./InterviewerList";
 
 import "../styles/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -64,7 +47,18 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
+  const [days, setDays] = useState([]);
 
+  useEffect(() => {
+    axios.get("/api/days")
+      .then((response) => {
+        console.log("response:", response.data)
+        setDays([...response.data])
+      })
+  }, [])
+
+  console.log("DAY:", day);
+  
   return (
     <main className="layout">
       <section className="sidebar">
