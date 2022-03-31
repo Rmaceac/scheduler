e9546c93e453e9079018a588ecc6b6d3fdc8adf1
Ryan <ryan.maceachern@gmail.com>
2022-03-30 21:36:30 -0700

    added additional cypress tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -5,16 +5,16 @@ describe("Appointments", () => {
     cy.contains("Monday");
   })
   
-  it("should book an interview" , () => {
+  xit("should book an interview" , () => {
     cy.get("[alt=Add]")
       .first()
       .click();
 
     cy.get("[data-testid=student-name-input]")
-      .type("Lydia Miller-Jones");
+      .type("Lydia Miller-Jones", {delay: 50});
 
     cy.get("[alt='Sylvia Palmer']")
-      // for some reason the html contains two Sylvia Palmers despite the interviewers having different names and photos
+      // for some reason the html from LHL contains two Sylvia Palmers despite the interviewers having different names and photos
       .first()
       .click();
 
@@ -25,5 +25,41 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
-  it ("should edit an interview")
+  xit ("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({force: true});
+
+    cy.get("[alt='Sylvia Palmer']")
+    // "Tori Malcolm" interviewer is named incorrectly within boilerplate test API from LHL
+    .eq(1)
+    .click();
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Ryan MacEachern", {delay: 100})
+
+    cy.contains("Save")
+      .click()
+
+    cy.contains(".appointment__card--show", "Ryan MacEachern");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  })
+
+  it ("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .first()
+      .click({force: true});
+
+    cy.contains("Confirm")
+      .click();
+
+    cy.contains("DELETING").should("exist");
+    cy.contains("DELETING").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+
+
+  })
 })
